57f443d13d9da0437c362883c0fc1baa2d0caa1b
pajith551@gmail.com <pajith551@gmail.com>
2022-03-27 23:53:14 +0530

    mongoose

--- app.js
+++ app.js
@@ -1,60 +1,88 @@
-const { MongoClient } = require("mongodb");
-const assert = require("assert");
-const { start } = require("repl");
+// Mongoose
 
-// Replace the uri string with your MongoDB deployment's connection string.
+const mongoose = require("mongoose");
 
-// const uri = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority";
+mongoose.connect("mongodb://localhost:27017/fruitsDB")
 
-// Connection URL
-const url = "mongodb://localhost:27017";
-
-// Database Name
-const dbName = "fruitsDB"
-
-//Create a new MongoClient
-const client = new MongoClient(url);
-
-//Use connection method to connect to the server
-client.connect(function(err){
-    assert.equal(null, err);
-    console.log("Connect successfully to the server")
-
-    const db = client.db(dbName);
-    findDocuments(db, function(){
-        client.close();
-    })
-    
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    rating: Number,
+    review: String
 });
 
-const insertDocuments = function(db, callback){
-    //get the document collection
-    const collection = db.collection("fruits");
-    //insert some document
-    collection.insertMany([
-        {
-            name: "Apple",
-            score: 8,
-            review: "Great fruit"
-        },
-        {
-            name: "Orange",
-            score: 6,
-            review: "Kind sour"
-        },
-        {
-            name: "Banana",
-            score: 9,
-            review: "Great stuff"
-        }
-    ], function(err, result){
-        assert.equal(err, null);
-        // assert.equal(3, result.result.n);
-        // assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
-        callback(result);
-    });
-};
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit({
+    name: "Apple",
+    rating: 7,
+    review: "Eat apple a day"
+});
+
+
+fruit.save()
+
+
+
+
+// Native MongoDB Driver
+
+// const { MongoClient } = require("mongodb");
+// const assert = require("assert");
+// const { start } = require("repl");
+
+// // Replace the uri string with your MongoDB deployment's connection string.
+
+// // const uri = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority";
+
+// // Connection URL
+// const url = "mongodb://localhost:27017";
+
+// // Database Name
+// const dbName = "fruitsDB"
+
+// //Create a new MongoClient
+// const client = new MongoClient(url);
+
+// //Use connection method to connect to the server
+// client.connect(function(err){
+//     assert.equal(null, err);
+//     console.log("Connect successfully to the server")
+
+//     const db = client.db(dbName);
+//     findDocuments(db, function(){
+//         client.close();
+//     })
+    
+// });
+
+// const insertDocuments = function(db, callback){
+//     //get the document collection
+//     const collection = db.collection("fruits");
+//     //insert some document
+//     collection.insertMany([
+//         {
+//             name: "Apple",
+//             score: 8,
+//             review: "Great fruit"
+//         },
+//         {
+//             name: "Orange",
+//             score: 6,
+//             review: "Kind sour"
+//         },
+//         {
+//             name: "Banana",
+//             score: 9,
+//             review: "Great stuff"
+//         }
+//     ], function(err, result){
+//         assert.equal(err, null);
+//         // assert.equal(3, result.result.n);
+//         // assert.equal(3, result.ops.length);
+//         console.log("Inserted 3 documents into the collection");
+//         callback(result);
+//     });
+// };
 
 const findDocuments = function(db, callback){
     //get the document collection
@@ -67,40 +95,3 @@ const findDocuments = function(db, callback){
         callback(fruits)
     })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// async function run() {
-//   try {
-//     await client.connect();
-
-//     const database = client.db('fruitsDB');
-//     const fruits = database.collection('fruits');
-
-//     // Query for a movie that has the title 'Back to the Future'
-//     const query = { title: 'Back to the Future' };
-//     const fruit = await fruits.findOne(query);
-
-//     console.log(fruit);
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
