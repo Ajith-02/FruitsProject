985f2dcc900771d27cdc250c5f181120a5762f72
pajith551@gmail.com <pajith551@gmail.com>
2022-03-27 18:26:02 +0530

    Find all document

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ client.connect(function(err){
     console.log("Connect successfully to the server")
 
     const db = client.db(dbName);
-    insertDocuments(db, function(){
+    findDocuments(db, function(){
         client.close();
     })
     
@@ -53,6 +53,18 @@ const insertDocuments = function(db, callback){
         // assert.equal(3, result.ops.length);
         console.log("Inserted 3 documents into the collection");
         callback(result);
+    });
+};
+
+const findDocuments = function(db, callback){
+    //get the document collection
+    const collection = db.collection("fruits");
+    //find some documents
+    collection.find({}).toArray(function(err, fruits){
+        assert.equal(err, null);
+        console.log("found the following records");
+        console.log(fruits);
+        callback(fruits)
     })
 }
 
